bd1de736b81ca8cd5c81e116003154cb70339a9e
gpfennig <pfenniger.griffin@gmail.com>
2018-10-30 14:29:37 +0000

    Working base layer

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const db = require('better-sqlite3')('restaurant.db');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const row = db.prepare('SELECT * FROM Customer').all();
+
+app.get('/', (req, res) => res.render("pages/index"));
+app.get('/test', (req, res) => res.send(row));
+
+app.listen(port, () => console.log('CPSC 2221 - Database Final Project'));
