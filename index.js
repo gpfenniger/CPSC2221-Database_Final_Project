8bf3d0eaa6734ac1433ec225aa58e45342744126
gpfennig <pfenniger.griffin@gmail.com>
2018-10-31 13:31:15 +0000

    base layout:

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.set('view engine', 'ejs');
 const row = db.prepare('SELECT * FROM Customer').all();
 
 app.get('/', (req, res) => res.render("pages/index"));
+app.get('/about', (req, res) => res.render("pages/about"));
+app.get('/search', (req, res) => res.render("pages/search"));
+app.get('/insert', (req, res) => res.render("pages/insert"));
 app.get('/test', (req, res) => res.send(row));
 
 app.listen(port, () => console.log('CPSC 2221 - Database Final Project'));
